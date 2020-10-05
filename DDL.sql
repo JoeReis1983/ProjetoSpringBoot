0cda6d6c66ea67568240769003f18aa466ad4699
JoeReis1983 <joeprimeiro@hotmail.com>
2020-10-05 13:33:09 +0000

    corrigindo essa bagaça

--- DDL.sql
+++ DDL.sql
@@ -11,7 +11,7 @@ grant select, insert, delete, update on dbserver.* to user@'localhost';
 create table clientes(
     id_cliente bigint unsigned not null auto_increment,
     nome_cliente varchar(20) not null,
-    primary key (id_produto),
+    primary key (id_cliente),
     primary key (nome_cliente),
     unique key uni_nome_cliente (nome_cliente)
 );
@@ -24,7 +24,14 @@ create table produtos(
 );
 create table pedidos(
     id_pedido bigint unsigned not null auto_increment,
-    foreign key nome_produto_pedido (nome_produto) references produtos (id_produto) on delete restrict on update cascade,
-    foreign key nome_produto_cliente (nome_cliente) references clientes (id_cliente) on delete restrict on update cascade
+    nome_produto varchar(20)  not null,
+    nome_cliente  varchar(20) not null,
+    id_cliente bigint unsigned not null,
+    id_produto bigint unsigned not null,
+    primary key(nome_produto,nome_cliente,id_cliente,id_produto),
+    foreign key nome_pedido_produto (nome_produto) references produtos (nome_produto) on delete restrict on update cascade,
+    foreign key nome_pedido_cliente (nome_cliente) references clientes (nome_cliente) on delete restrict on update cascade
+    foreign key id_pedido_cliente (id_cliente) references clientes (id_cliente) on delete restrict on update cascade
+    foreign key id_pedido_produto (id_produtos) references clientes (id_produtos) on delete restrict on update cascade
     
 );
